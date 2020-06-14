61162b9d5cd57efa84ef5dc1b06db376438a11eb
Chinmay Shah <cjshah2000@gmail.com>
2020-06-14 12:28:41 +0530

    Added BODMAS. Resolved many input exceptions.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -39,46 +41,124 @@ namespace SimpleCalculator
             string output = default;
             if (IsInputCorrect(input))
             {
-                char[] separators = { '+', '-', '/', '*' };
-                string[] Nums = input.Split(separators);
-                int index = 0;
-                float Result = 0;
-                foreach (var s in Nums)
+                //Step 1: Parse to postfix from infix:
+                Stack<char> operands = new Stack<char>();
+                string PostFix = "";
+                for(int i=0; i<input.Length; i++)
                 {
-                    float num = 0;
-                    float.TryParse(s, out num);
-                    index = input.IndexOf(s, index);
-                    if (index == 0)
-                        Result += num;
-                    else
+                    if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
                     {
-                        switch (input[index - 1])
+                        PostFix += ',';
+                        if (operands.Count == 0 || Precedence(input[i]) > Precedence(operands.Peek()))
+                            operands.Push(input[i]);
+                        else
                         {
-                            case '+':
-                                Result += num;
-                                break;
-                            case '-':
-                                Result -= num;
-                                break;
-                            case '*':
-                                Result *= num;
-                                break;
-                            case '/':
-                                Result /= num;
-                                break;
+                            while (operands.Count != 0 && Precedence(operands.Peek()) > Precedence(input[i]))
+                                PostFix += operands.Pop();
+                            operands.Push(input[i]);
                         }
                     }
-                    index += s.Length;
+                    else PostFix += input[i];
                 }
-                output = Result.ToString();
+
+                while (operands.Count != 0)
+                    PostFix += operands.Pop();
+
+                //Step 2: Parse from postfix to output
+                Stack<float> Output = new Stack<float>();
+                StringBuilder num = new StringBuilder();
+                for (int i=0; i<PostFix.Length; i++)
+                {
+                    if(PostFix[i] == ',')
+                    {
+                        if (float.TryParse(num.ToString(), out float result))
+                            Output.Push(result);
+                        else return "Input Error!";
+                        num.Clear();
+                    }
+
+                    else if (PostFix[i] == '+' || PostFix[i] == '-' || PostFix[i] == '*' || PostFix[i] == '/')
+                    {
+                        if(num.Length != 0)
+                        {
+                            if (float.TryParse(num.ToString(), out float result))
+                                Output.Push(result);
+                            else return "Input Error!";
+                            num.Clear();
+                      
+                                float op = Output.Pop();
+                                switch (PostFix[i])
+                                {
+                                    case '+':
+                                        op += Output.Pop();
+                                        break;
+
+                                    case '-':
+                                        op = Output.Pop() - op;
+                                        break;
+
+                                    case '*':
+                                        op *= Output.Pop();
+                                        break;
+
+                                    case '/':
+                                        op = Output.Pop() / op;
+                                        break;
+
+                                }
+                                Output.Push(op);
+                    
+                        }
+
+                        else
+                        {
+                            float op = Output.Pop();  
+                            switch (PostFix[i])
+                            {
+                                case '+':
+                                    op += Output.Pop();
+                                    break;
+
+                                case '-':
+                                    op = Output.Pop() - op;
+                                    break;
+                               
+                                case '*':
+                                    op *= Output.Pop();
+                                    break;
+                                
+                                case '/':
+                                    op = Output.Pop() / op;
+                                    break;
+
+                            }
+                            Output.Push(op);
+                        }
+                    }
+
+                    else
+                    {
+                        num.Append(PostFix[i]);       
+                    }
+                }
+
+                output = Output.Peek().ToString();
             }
 
             else output = "Likhte bhi nahi aata!";
             return output;
         }
 
+        int Precedence (char op)
+        {
+            if (op == '/' || op == '*')
+                return 2;
+            else return 1;
+        }
+
         bool IsInputCorrect(string input)
         {
+            //to check whether the expression is incomplete
             int indexPlus = input.LastIndexOf('+');
             if (indexPlus == input.Length - 1)
                 return false;
@@ -92,21 +172,34 @@ namespace SimpleCalculator
             if (indexDivide == input.Length - 1)
                 return false;
 
+            //to check if there's any division by zero
+            //also checks if two operators don't have an operand in between or the numeric input is inappropriate
+            //also checks if the first char of input is not a number
             char[] separators = { '+', '-', '/', '*' };
             string[] Nums = input.Split(separators);
+            if (input.IndexOf(Nums[0]) != 0)
+                return false;
             for (var i=1; i<Nums.Length; i++)
             {   float Number = default;
-                float.TryParse(Nums[i], out Number);
-                if (Number == (float)0 && input[input.IndexOf(Nums[i]) - 1] == '/')
-                    return false;
+                if (float.TryParse(Nums[i], out Number))
+                {
+                    if (Number == (float)0 && input[input.IndexOf(Nums[i]) - 1] == '/')
+                        return false;
+                }
+                else return false;
             }
             return true;
         }
 
         private void ClickBack(object sender, RoutedEventArgs e)
         {
-            string newDisplay = Display.Text.Substring(0, Display.Text.Length - 1);
-            Display.Text = newDisplay;
+            if (Display.Text.Length != 0)
+            {
+                string newDisplay = Display.Text.Substring(0, Display.Text.Length - 1);
+                Display.Text = newDisplay;
+            }
+
+            else ;
         }
 
         private void ClickClear(object sender, RoutedEventArgs e)
